fc32d5139bb19a46606aa9f2885a826563d7dded
tbolp <tbolp@qq.com>
2021-12-30 18:21:20 +0800

    develop

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"regexp"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -19,11 +20,12 @@ import (
 )
 
 type Device struct {
-	UDN     string
-	Name    string
-	URLBase string
-	CtrlURL string
-	Expired time.Time
+	UDN      string
+	Name     string
+	URLBase  string
+	CtrlURL  string
+	LocalURL string
+	Expired  time.Time
 }
 
 type CastService struct {
@@ -33,6 +35,7 @@ type CastService struct {
 	group         sync.WaitGroup
 	is_search     bool
 	select_device Device
+	file_path     string
 }
 
 func (this *CastService) Init() {
@@ -45,12 +48,13 @@ func (this *CastService) Init() {
 	this.conn = conn
 	this.is_search = true
 	go this.searchDevices()
+	go this.httpServer()
 	this.group.Add(1)
 	atomic.StoreInt32(&this.status, 1)
 }
 
 func (this *CastService) Stop() {
-	this.is_search = false
+	// this.is_search = false
 	this.group.Wait()
 }
 
@@ -71,8 +75,10 @@ func (this *CastService) searchDevices() {
 			IP:   net.ParseIP("239.255.255.250"),
 		})
 		this.conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-		_, _, err := this.conn.ReadFrom(resp)
+		_, addr, err := this.conn.ReadFrom(resp)
 		if err == nil {
+			c, _ := net.DialUDP("udp", nil, addr.(*net.UDPAddr))
+			local_addr := strings.Split(c.LocalAddr().String(), ":")[0]
 			httpresp, err := http.ReadResponse(bufio.NewReader(bytes.NewReader(resp)), nil)
 			if err != nil {
 				continue
@@ -112,11 +118,12 @@ func (this *CastService) searchDevices() {
 						re, _ := regexp.Compile("SetAVTransportURI")
 						if re.Find(content) != nil {
 							dev := Device{
-								UDN:     root.UDN,
-								Name:    root.Name,
-								URLBase: root.URLBase,
-								CtrlURL: service.CtrlURL,
-								Expired: time.Now().Add(time.Second * time.Duration(max_age)),
+								UDN:      root.UDN,
+								Name:     root.Name,
+								URLBase:  root.URLBase,
+								CtrlURL:  service.CtrlURL,
+								LocalURL: local_addr,
+								Expired:  time.Now().Add(time.Second * time.Duration(max_age)),
 							}
 							if v, ok := this.devices[root.UDN]; ok {
 								if v.Expired.After(time.Now()) {
@@ -136,15 +143,15 @@ func (this *CastService) searchDevices() {
 func (this *CastService) httpServer() {
 	engine := gin.Default()
 	engine.GET("/file", func(c *gin.Context) {
-
+		c.File(this.file_path)
 	})
-	engine.Run(":")
+	engine.Run(":12345")
 }
 
 func (this *CastService) ListDevices() []Device {
 	res := []Device{}
 	for k, v := range this.devices {
-		if v.Expired.After(time.Now()) {
+		if v.Expired.Before(time.Now()) {
 			delete(this.devices, k)
 		}
 		res = append(res, v)
@@ -165,11 +172,61 @@ func (this *CastService) SelectDevice(UDN string) bool {
 }
 
 func (this *CastService) CastFile(path string) {
+	this.file_path = path
+	cur_ip := this.select_device.LocalURL
+	type Action struct {
+		Xmlns              string `xml:"xmlns:u,attr"`
+		InstanceID         int
+		CurrentURI         string
+		CurrentURIMetaData string
+	}
+	type Body struct {
+		ActionName Action `xml:"u:SetAVTransportURI"`
+	}
+	type Envelope struct {
+		XMLName       xml.Name `xml:"s:Envelope"`
+		Xmlns         string   `xml:"xmlns:s,attr"`
+		EncodingStyle string   `xml:"s:encodingStyle,attr"`
+		Body          Body     `xml:"s:Body"`
+	}
 
+	envelop := Envelope{}
+	envelop.Xmlns = "http://schemas.xmlsoap.org/soap/envelope/"
+	envelop.EncodingStyle = "http://schemas.xmlsoap.org/soap/encoding/"
+	envelop.Body.ActionName.Xmlns = "urn:schemas-upnp-org:service:AVTransport:1"
+	envelop.Body.ActionName.InstanceID = 0
+	// envelop.Body.ActionName.CurrentURI = "https://www.baidu.com/img/flexible/logo/pc/result.png"
+	envelop.Body.ActionName.CurrentURI = fmt.Sprintf("http://%s:12345/file", cur_ip)
+	data, err := xml.MarshalIndent(&envelop, " ", "  ")
+	// data, err := xml.Marshal(&envelop)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// fmt.Println(string(data))
+	// data = append([]byte("<?xml version=\"1.0\"?>\r\n"), data...)
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/%s", this.select_device.URLBase, this.select_device.CtrlURL), bytes.NewReader(data))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	req.Header.Set("Content-Type", "text/xml; charset=\"utf-8\"")
+	req.Header.Set("SOAPACTION", "\"urn:schemas-upnp-org:service:AVTransport:1#SetAVTransportURI\"")
+	data, _ = httputil.DumpRequest(req, true)
+	fmt.Println(string(data))
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	data, _ = ioutil.ReadAll(resp.Body)
+	fmt.Println(string(data))
 }
 
 func main() {
 	srv := CastService{}
 	srv.Init()
+	for len(srv.ListDevices()) == 0 {
+		fmt.Println("test")
+	}
+	srv.SelectDevice(srv.ListDevices()[0].UDN)
+	srv.CastFile("/home/tbolp/Pictures/icon.jpeg")
 	srv.Stop()
 }
